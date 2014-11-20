ab581c1c6b1a78371673a7c8c48a23cd0ed9e917
ajturner <aturner@esri.com>
2014-11-19 22:19:55 -0500

    Added support for Feature Service query

--- cedar.js
+++ cedar.js
@@ -1,13 +1,39 @@
 
 var Cedar = Cedar || {}
 
+
+Cedar.query = function(params) {
+  options = {
+    where: "1=1",
+    returnGeometry: false,
+    returnDistinctValues: false,
+    returnIdsOnly: false,
+    returnCountOnly: false,
+    outFields: "*",
+    f: "json"
+  }
+  if(params.group) {
+    options.groupByFieldsForStatistics = params.group;
+  }
+  if(params.count) {
+    options.orderByFields = params.count + "_SUM";
+    options.outStatistics = JSON.stringify([
+      {"statisticType": "sum", "onStatisticField": params.count, "outStatisticFieldName": params.count + "_SUM"}]);
+  }
+  return params.url + "/query?" + Cedar.serialize(options);
+}
+
 Cedar.chart = function(params) {
+  params.data = params.data || Cedar.query(params);
+  console.log(params)
   d3.json(params.style + ".json", function(spec) {
     var chart = JSON.stringify(spec).supplant(params);
     Cedar.parse(params.el, JSON.parse(chart));
   });
 }
-  
+
+console.log(url)
+
 Cedar.parse = function(el, spec) {
   vg.parse.spec(spec, function(chart) { console.log(el, spec); chart({el: el}).update(); });
 }
@@ -21,3 +47,12 @@ String.prototype.supplant = function (o) {
         }
     );
 };
+
+Cedar.serialize = function(obj) {
+  var str = [];
+  for(var p in obj)
+    if (obj.hasOwnProperty(p)) {
+      str.push(encodeURIComponent(p) + "=" + encodeURIComponent(obj[p]));
+    }
+  return str.join("&");
+}
