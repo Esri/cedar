813031ccb8219bc364783d29d8db7cb5db21a5fa
ajturner <aturner@esri.com>
2015-07-06 22:00:00 -0400

    Updates to travis for deploy script

--- deploy.sh
+++ deploy.sh
@@ -6,7 +6,6 @@ if [ "$TRAVIS_BRANCH" == "master" ]; then
 	rm -rf site/build || exit 0;
 	mkdir -p site/build;
 
-	npm install -g grunt-cli
 	bower install
 	# run our compile script, discussed above
 	grunt docs:build
