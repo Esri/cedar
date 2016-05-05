2ac173c86cb603ada66a71ba67f505fe00efd7cd
Ben Stoltz <bstoltz@esri.com>
2016-05-04 23:09:03 -0400

    Change deploy.sh order

---
+++ scripts/deploy.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+#
+set -o errexit -o nounset
+
+if [ "$TRAVIS_BRANCH" != "develop" ]
+ then
+	 echo "This commit was made against the $TRAVIS_BRANCH. No deploy!"
+	 exit 0
+ fi
+ set -e # exit with nonzero exit code if anything fails
+
+ # clear and re-create the out directory
+ rm -rf site/build || exit 0;
+ mkdir -p site/build;
+
+ npm install
+ # run our compile script, discussed above
+ grunt docs:build
+
+ # go to the out directory and create a *new* Git repo
+ cd site/build
+ git init
+
+ # inside this git repo we'll pretend to be a new user
+ git config user.name "Andrew Turner"
+ git config user.email "aturner@esri.com"
+
+ # The first and only commit to this new Git repo contains all the
+ # files present with the commit message "Deploy to GitHub Pages".
+ git add .
+ git commit -m "Deploy to GitHub Pages"
+
+ # Force push from the current repo's master branch to the remote
+ # repo's gh-pages branch. (All previous history on the gh-pages branch
+ # will be lost, since we are overwriting it.) We redirect any output to
+ # /dev/null to hide any sensitive credential data that might otherwise be exposed.
+ git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" master:gh-pages > /dev/null 2>&1
