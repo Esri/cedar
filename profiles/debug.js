82c3a349a8fc64f63ab63ad248d917d9631cd50d
Tom Wayson <tom@tomwayson.com>
2016-09-29 10:02:10 -0700

    run docs site off of rollup output
    also updated bar example page to get d3/vega from CDN

--- profiles/debug.js
+++ profiles/debug.js
@@ -1,6 +1,6 @@
 import config from './base';
 
-config.dest = 'dist/cedar-test-debug.js';
+config.dest = 'dist/cedar-debug.js';
 config.sourceMap = 'inline';
 
 export default config;
--- profiles/production.js
+++ profiles/production.js
@@ -1,8 +1,8 @@
 import config from './base';
 import uglify from 'rollup-plugin-uglify';
 
-config.dest = 'dist/cedar-test.js';
-config.sourceMap = 'dist/cedar-test.js.map';
+config.dest = 'dist/cedar.js';
+config.sourceMap = 'dist/cedar.js.map';
 
 // use a Regex to preserve copyright text
 config.plugins.push(uglify({ output: {comments: /Institute, Inc/} }));
