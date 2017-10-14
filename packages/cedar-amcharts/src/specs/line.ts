53d23d147df7fcb43362d14f6f48b577ece73fda
ajturner <aturner@esri.com>
2017-10-14 16:17:34 -0400

    Added Calcite theme

--- packages/cedar-amcharts/src/specs/line.ts
+++ packages/cedar-amcharts/src/specs/line.ts
@@ -1,42 +1,24 @@
 const line = {
   type: 'serial',
-  theme: 'light',
-  graphs: [{
-    fillAlphas: 0,
-    lineAlpha: 1,
-    dashLengthField: 'dashLengthLine',
-    useLineColorForBulletBorder: true,
-    bulletBorderThickness: 3,
-    bullet: 'circle',
-    bulletBorderAlpha: 0.8,
-    bulletAlpha: 0.8,
-    bulletColor: '#FFFFFF',
-  }],
-  legend: {
-    horizontalGap: 10,
-    position: 'bottom',
-    useGraphSettings: true,
-    markerSize: 10
-  },
-  valueAxes: [ {
-    gridColor: '#FFFFFF',
-    gridAlpha: 0.2,
-    dashLength: 0
-  } ],
-  gridAboveGraphs: true,
-  startDuration: 0.1,
-  startEffect: 'easeInSine',
+  theme: 'calcite',
   chartCursor: {
     categoryBalloonEnabled: false,
-    cursorAlpha: 0,
-    zoomable: false
+    valueLineBalloonEnabled: true
   },
-  categoryAxis: {
-    gridPosition: 'start',
-    gridAlpha: 0,
-    tickPosition: 'start',
-    tickLength: 20,
-    guides: []
+  graphs: [{
+    bullet: 'circle',
+    bulletAlpha: 1,
+    bulletBorderAlpha: 0.8,
+    bulletBorderThickness: 0,
+    // bulletColor: '#FFFFFF',
+    dashLengthField: 'dashLengthLine',
+    fillAlphas: 0,
+    useLineColorForBulletBorder: true
+  }],
+  legend: {
+    // horizontalGap: 10,
+    position: 'bottom',
+    useGraphSettings: true
   },
   export: {
     enabled: true
