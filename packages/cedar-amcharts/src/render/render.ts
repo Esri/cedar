c52e431c2484e9304c64af34f9eb60115c0c61d4
Ben Stoltz <bstoltz@esri.com>
2017-12-13 13:46:57 -0500

    Will support grouped as a type and default behavior of multiple series data is to group rather than stack

--- packages/cedar-amcharts/src/render/render.ts
+++ packages/cedar-amcharts/src/render/render.ts
@@ -67,8 +67,8 @@ export function fillInSpec(spec: any, definition: any) {
         spec.valueField = graph.valueField
 
         // Group vs. stack
-        if (!!series.group) {
-          graph.newStack = true
+        if (!!series.stack) {
+          graph.newStack = false
         }
 
         // x/y types, scatter, bubble
@@ -105,6 +105,8 @@ export function fetchSpec(type: string): any {
   } else if (spec === 'bubble') {
     console.warn(`'bubble' is no longer a supported type. Please use 'scatter' instead`)
     spec = 'scatter'
+  } else if (spec === 'grouped') {
+    spec = 'bar'
   }
   return clone(specs[spec])
 }
--- packages/cedar-amcharts/src/specs/bar.ts
+++ packages/cedar-amcharts/src/specs/bar.ts
@@ -7,6 +7,7 @@ export const bar = {
   },
   graphs: [{
     type: 'column',
+    newStack: true
   }],
   legend: {},
   valueAxes: [ {
