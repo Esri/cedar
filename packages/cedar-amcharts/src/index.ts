08bd72f9226d64cc270c2c182d8e3b5a221b7177
Ben Stoltz <bstoltz@esri.com>
2018-02-27 09:44:12 -0500

    references to spec that should be towards definition fixed

--- packages/cedar-amcharts/src/index.ts
+++ packages/cedar-amcharts/src/index.ts
@@ -1,15 +1,15 @@
 import render from './render/render'
 
-export function cedarAmCharts(elementId: string, spec: any, data?: any[]) {
-  if ((!elementId || !spec || !data) && (spec.type && spec.type !== 'custom')) {
-    const err = new Error('Element Id, specification, and data are all required.')
+export function cedarAmCharts(elementId: string, definition: any, data?: any[]) {
+  if ((!elementId || !definition || !data) && (definition.type && definition.type !== 'custom')) {
+    const err = new Error('Element Id, definitionification, and data are all required.')
     throw err
   }
 
-  if (spec.type && spec.type === 'custom') {
-    return render.renderChart(elementId, spec)
+  if (definition.type && definition.type === 'custom') {
+    return render.renderChart(elementId, definition)
   }
-  return render.renderChart(elementId, spec, data)
+  return render.renderChart(elementId, definition, data)
 }
 
 export default cedarAmCharts
