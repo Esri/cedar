9ff910b29dd559727e926ad37731b3d1e72cb85f
Ben Stoltz <bstoltz@esri.com>
2018-03-14 10:17:10 -0400

    Changes based on toms feedback, tests incoming

--- packages/cedar/src/flatten/flatten.ts
+++ packages/cedar/src/flatten/flatten.ts
@@ -6,29 +6,21 @@ export interface IFeatureSet {
   features: IFeature[]
 }
 
-export function handleArrOfData(data: any[]) {
-  return data.map((feat) => {
-    if (feat.attributes) { return feat }
-    if (feat !== null && typeof feat === 'object') {
-      return {
-        attributes: feat
-      }
-    }
-  })
-}
-
 function flattenFeature(feature: IFeature, categoryField?: string) {
-  const attributes = feature.attributes
+  const attributes = feature.attributes ? feature.attributes : feature
   if (categoryField) {
     // render function expects a property named 'categoryField'
     // TODO: what if attributes already has a 'categoryField' property
     attributes['categoryField'] = attributes[categoryField] || ''
   }
-  return feature.attributes
+  return attributes
 }
 
 // just return an array of flattened features
 export function flattenFeatureSet(featureSet: IFeatureSet, categoryField?) {
+  if (Array.isArray(featureSet)) {
+    return featureSet.map((feature) => (flattenFeature(feature, categoryField)))
+  }
   return featureSet.features.map((feature) => (flattenFeature(feature, categoryField)))
 }
 
@@ -36,10 +28,11 @@ export function flattenFeatureSet(featureSet: IFeatureSet, categoryField?) {
 export function buildIndex(joinKeys: string[], featureSets: any[]) {
   const index = {}
   featureSets.forEach((featureSet, i) => {
+    if (featureSet && Array.isArray(featureSet)) { featureSet = { features: featureSet } }
     featureSet.features.forEach((feature) => {
       // the column that this dataset should be joined on
       const joinKey = joinKeys[i]
-      const joinValue = feature.attributes[joinKeys[i]]
+      const joinValue = feature.attributes ? feature.attributes[joinKeys[i]] : feature[joinKeys[i]]
       if (index[joinValue] === undefined) {
         index[joinValue] = []
       }
@@ -71,7 +64,7 @@ export function flattenFeatureSets(featureSets: any[], joinKeys: any[]) {
     const features = index[uniqueValue]
     const row = { categoryField: features[0].attributes[key] || '' }
     features.forEach((feature, i) => {
-      const attributes = feature.attributes
+      const attributes = feature.attributes ? feature.attributes : feature
       Object.keys(attributes).forEach((attrKey) => {
         // append the feature's index to the row's column name
         const col = `${attrKey}_${i}`
--- packages/cedar/test/flatten/flatten.spec.ts
+++ packages/cedar/test/flatten/flatten.spec.ts
@@ -1,5 +1,5 @@
 import {} from 'jest'
-import { buildIndex, flattenFeatureSets, handleArrOfData } from '../../src/flatten/flatten'
+import { buildIndex, flattenFeatureSets } from '../../src/flatten/flatten'
 import expectedChartData from '../data/chartData'
 import featureServiceResponse from '../data/featureServiceResponse'
 import schoolResponse from '../data/schoolResponse'
@@ -35,104 +35,5 @@ describe('running when a single dataset', () => {
     }
 
     expect(buildIndex(data.joinKeys, data.featureSets)).toEqual(result)
-  })
-
-  test('flattenFeatureSets properly flattens when provided join keys', () => {
-    const data = {
-      joinKeys: ['Type', 'Type', 'Type'],
-      featureSets: schoolResponse
-    }
-
-    const result = [
-      {
-        categoryField: 'High School',
-        Number_of_SUM_0: 13,
-        Type_0: 'High School',
-        Number_of_SUM_1: 8,
-        Type_1: 'High School'
-      },
-      {
-        categoryField: 'Middle School',
-        Number_of_SUM_0: 6,
-        Type_0: 'Middle School',
-        Number_of_SUM_1: 0,
-        Type_1: 'Middle School'
-      },
-      {
-        categoryField: 'Elementary School',
-        Number_of_SUM_0: 1,
-        Type_0: 'Elementary School',
-        Number_of_SUM_1: 1,
-        Type_1: 'Elementary School',
-        Number_of_SUM_2: 1,
-        Type_2: 'Elementary School'
-      }
-    ]
-
-    expect(flattenFeatureSets(data.featureSets, data.joinKeys)).toEqual(result)
-  })
-
-  test('handleArrOfData properly handles featureResponse.features', () => {
-    const data = [
-      {
-        attributes: {
-          Number_of_SUM: 13,
-          Type: 'High School'
-        }
-      },
-      {
-        attributes: {
-          Number_of_SUM: 6,
-          Type: 'Middle School'
-        }
-      },
-      {
-        attributes: {
-          Number_of_SUM: 1,
-          Type: 'Elementary School'
-        }
-      }
-    ]
-
-    expect(handleArrOfData(data)).toEqual(data)
-  })
-
-  test('handleArrOfData properly handles array of non-attribute data', () => {
-    const data = [
-      {
-        Number_of_SUM: 13,
-        Type: 'High School'
-      },
-      {
-        Number_of_SUM: 6,
-        Type: 'Middle School'
-      },
-      {
-        Number_of_SUM: 1,
-        Type: 'Elementary School'
-      }
-    ]
-
-    const result = [
-      {
-        attributes: {
-          Number_of_SUM: 13,
-          Type: 'High School'
-        }
-      },
-      {
-        attributes: {
-          Number_of_SUM: 6,
-          Type: 'Middle School'
-        }
-      },
-      {
-        attributes: {
-          Number_of_SUM: 1,
-          Type: 'Elementary School'
-        }
-      }
-    ]
-    expect(handleArrOfData(data)).toEqual(result)
   })
 })
